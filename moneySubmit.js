f037a9ad8e3f328955cac934394506abb7a321dc
vbanurag <asharma.soccer@gmail.com>
2017-03-08 11:41:40 +0530

    Code revision

--- moneySubmit.js
+++ moneySubmit.js
@@ -1,20 +1,23 @@
 function isNumber(str) {
-    var pattern = /^\d+$/;
-    return pattern.test(str);  // returns a boolean
+        var pattern = /^\d+$/;
+        return pattern.test(str);  // returns a boolean
+    
 }
+
 function printError(str){
   $("#error").text(str);
 }
+
 function getValue(){
 	var twoThousand = document.getElementById("twoThousand").value;
 	var fiveHundred = document.getElementById("fiveHundred").value;
 	var oneHundred = document.getElementById("oneHundred").value;
 	var maxAmount =  document.getElementById("maxAmount").value;
-  var valid = true;
+  	var valid = true;
 	if(isNumber(twoThousand)==false)
 	{
 		printError(twoThousand+ "  Invalid Value");
-    valid = false;
+    	valid = false;
 	}
 	else if(isNumber(fiveHundred)==false)
 	{
@@ -22,9 +25,8 @@ function getValue(){
     valid = false;
 
 	}
-	else if(isNumber(oneHundred)==false)
-	{
-		printError(oneHundred + " Invalid Value");
+	else if( isNumber(oneHundred)==false){
+	printError(oneHundred + " Invalid Value");
     valid = false;
 
 	}
@@ -33,48 +35,43 @@ function getValue(){
     valid = false;
 
 	}
-
-
+	
+	
+	if(valid){
+	     var startATM = new initATM(parseInt(twoThousand),parseInt(fiveHundred),parseInt(oneHundred),parseInt(maxAmount));
+	     $("#deposit").hide();
+	     startATM.deduct();
+    	 
+  	}
+	
 	//alert(amnt);
-  if(valid){
-    initATM(parseInt(twoThousand),parseInt(fiveHundred),parseInt(oneHundred),parseInt(maxAmount));
-    $("#deposit").hide();
-  }
 	//finalValue(parseInt(twoThousand),parseInt(fiveHundred),parseInt(oneHundred))
+	
+}
+
+
+
+function moneySubmit(twoThousand,fiveHundred,oneHundred){
+	this.twoThousand=twoThousand;
+	this.fiveHundred=fiveHundred;
+	this.oneHundred=oneHundred;
+
+	this.currentAmount = 0;
+
+	var currentAmnt = function(){
+		this.currentAmount = (twoThousand*2000)+(fiveHundred*500)+(oneHundred*100)
+		return currentAmount;
+	}
+	this.currentAmount= currentAmnt();
+
 
 }
 
-//
-//
-// function moneySubmit(twoThousand,fiveHundred,oneHundred){
-// 	this.twoThousand=twoThousand;
-// 	this.fiveHundred=fiveHundred;
-// 	this.oneHundred=oneHundred;
-//
-// 	this.currentAmount = 0;
-//
-// 	var currentAmnt = function(){
-// 		this.currentAmount = (twoThousand*2000)+(fiveHundred*500)+(oneHundred*100)
-// 		return currentAmount;
-// 	}
-// 	this.currentAmount= currentAmnt();
-//
-//
-// }
-//
-// //To print data on the html
-// function finalAmount(amount){
-// 	alert(amount);
-// 	$(".finalAmountDisplay").html(amount);
-//
-//
-// }
+//To print data on the html
+function finalAmount(amount){
+	$(".finalAmountDisplay").html(amount);
 
-//function for final Value
-function finalValue(twoThousand,fiveHundred,oneHundred){
-	var mon = new moneySubmit(twoThousand,fiveHundred,oneHundred);
-	var amnt = mon.currentAmount;
-	//alert(amnt);
-	finalAmount(amnt);
 
 }
+
+
---
+++ withdrawModule_class.js
@@ -0,0 +1,80 @@
+function initATM(n_2000,n_500,n_100,max){
+  this.notes_2000 = 0;
+  this.notes_500  = 0;
+  this.notes_100  = 0;
+  this.maxLimit = 0;
+  this.notes_2000 = n_2000;
+  this.notes_500 = n_500;
+  this.notes_100 = n_100;
+  this.maxLimit = max;
+  this.amountDeposit = this.notes_2000*2000 + this.notes_500*500 + this.notes_100*100;
+
+  var logEntry = "<tr class='deposit'><td>" + amountDeposit + "</td><td>" + notes_2000 + "</td><td>" + notes_500 + "</td><td>" + notes_100 + "</td><td>" + amountDeposit +"</td</tr>";
+  
+  var updateLog=function(str){
+  $('#someTable').append(str);
+  }
+
+  this.deduct = function() {
+      var val = document.getElementById("withdrawamount").value;
+      if(isNumber(val)){
+        var amountWithdraw = parseInt(val);
+        var temp_notes_2000 = 0;
+        var temp_2000 = 0;
+        var temp_notes_500 = 0;
+        var temp_500 = 0;
+        var temp__notes_100 = 0;
+        var temp_100 = 0;
+        var tempAmount = amountWithdraw;
+            if(amountWithdraw>this.maxLimit){
+                printError("Sorry, Please enter amount less than " + this.maxLimit + "!!!!!");
+            }else{
+
+              if(tempAmount < 100 || (tempAmount %100 != 0)){
+                printError("Please enter the amount in the multiple of 100");
+              }else{
+                var despense_2000 = parseInt(tempAmount/2000);
+
+                if(this.notes_2000 >= despense_2000){
+                  temp_notes_2000 = this.notes_2000 - despense_2000;
+                  temp_2000 = 0;
+                }else{
+                  temp_2000 = despense_2000 - this.notes_2000;
+                }
+                tempAmount = tempAmount%2000 + temp_2000 * 2000;
+
+                var despense_500 = parseInt(tempAmount/500);
+                if(this.notes_500 >= despense_500){
+                  temp_notes_500 = this.notes_500 - despense_500;
+                  temp_500 = 0;
+                }else{
+                  temp_500 = despense_500 - this.notes_500;
+                }
+                tempAmount = tempAmount%500 + temp_500 * 500;
+
+                var despense_100 = parseInt(tempAmount/100);
+
+                if(this.notes_100 >= despense_100){
+                  this.notes_100 = this.notes_100 - despense_100;
+                  this.notes_500 = temp_notes_500;
+                  this.notes_2000 = temp_notes_2000;
+                  var logEntry = "<tr class='withdraw'><td>" + amountWithdraw + "</td><td>" + this.notes_2000 + "</td><td>" + this.notes_500 + "</td><td>" + this.notes_100 + "</td><td>" + (this.notes_2000*2000 + this.notes_500*500 + this.notes_100*100) +"</td</tr>";
+                  updateLog(logEntry);
+                  printError("Collect Your Amount!!!!!!!!!!");
+                  $("error").addClass("setGreen");
+                }else{
+                  printError("Not Enough Cash!!!!!!!!!!");
+                }
+              }
+            }
+      }else{
+        $("#error").text("Invalid Amount!!!!!!!");
+      }
+}
+
+  updateLog(logEntry);
+  printError("");
+
+}
+
+
